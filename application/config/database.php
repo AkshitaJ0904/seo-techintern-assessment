1bd1e67209e9676d236da87b7cf611377a736e3d
Dhanush kumar <dhanush.23cse@bmu.edu.in>
2025-04-23 22:20:46 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -4,14 +4,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-// Try using the environment variables directly
+// Get the database URL from environment variables
+$db_url = getenv('MYSQL_URL') ?: 'mysql://root:eBBazdhuCQD1drrVvCoRuJCNjmzTBbXO@trolley.proxy.rlwy.net:13292/railway';
+
+// Parse the database URL
+$parsed_url = parse_url($db_url);
+
 $db['default'] = array(
     'dsn'      => '',
-    'hostname' => getenv('MYSQLHOST') ?: 'trolley.proxy.rlwy.net',
-    'username' => getenv('MYSQLUSER') ?: 'root',
-    'password' => getenv('MYSQLPASSWORD') ?: 'eBBazdhuCQD1drrVvCoRuJCNjmzTBbXO',
-    'database' => getenv('MYSQL_DATABASE') ?: 'railway',
-    'port'     => getenv('MYSQLPORT') ?: '13292',
+    'hostname' => isset($parsed_url['host']) ? $parsed_url['host'] : 'trolley.proxy.rlwy.net',
+    'username' => isset($parsed_url['user']) ? $parsed_url['user'] : 'root',
+    'password' => isset($parsed_url['pass']) ? $parsed_url['pass'] : 'eBBazdhuCQD1drrVvCoRuJCNjmzTBbXO',
+    'database' => isset($parsed_url['path']) ? trim($parsed_url['path'], '/') : 'railway',
+    'port'     => isset($parsed_url['port']) ? $parsed_url['port'] : '13292',
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
     'pconnect' => FALSE,
@@ -25,6 +30,5 @@ $db['default'] = array(
     'compress' => FALSE,
     'stricton' => FALSE,
     'failover' => array(),
-    'save_queries' => TRUE,
-    'socket'   => '' // Make sure this is empty to force TCP/IP connection
+    'save_queries' => TRUE
 );
