c07b708d1c9e9ed4cd179bafebba3de08a00da54
Dhanush kumar <dhanush.23cse@bmu.edu.in>
2025-04-23 22:22:09 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -4,19 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-// Get the database URL from environment variables
-$db_url = getenv('MYSQL_URL') ?: 'mysql://root:eBBazdhuCQD1drrVvCoRuJCNjmzTBbXO@trolley.proxy.rlwy.net:13292/railway';
-
-// Parse the database URL
-$parsed_url = parse_url($db_url);
-
 $db['default'] = array(
     'dsn'      => '',
-    'hostname' => isset($parsed_url['host']) ? $parsed_url['host'] : 'trolley.proxy.rlwy.net',
-    'username' => isset($parsed_url['user']) ? $parsed_url['user'] : 'root',
-    'password' => isset($parsed_url['pass']) ? $parsed_url['pass'] : 'eBBazdhuCQD1drrVvCoRuJCNjmzTBbXO',
-    'database' => isset($parsed_url['path']) ? trim($parsed_url['path'], '/') : 'railway',
-    'port'     => isset($parsed_url['port']) ? $parsed_url['port'] : '13292',
+    'hostname' => 'trolley.proxy.rlwy.net',  // Use the proxy hostname
+    'username' => 'root',
+    'password' => 'eBBazdhuCQD1drrVvCoRuJCNjmzTBbXO',
+    'database' => 'railway',
+    'port'     => '13292',  // Use the proxy port
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
     'pconnect' => FALSE,
