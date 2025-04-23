fdb3bdfd9ba3e9b4f231b48ff3bbfe665bd4a03c
Dhanush kumar <dhanush.23cse@bmu.edu.in>
2025-04-23 22:02:42 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -5,12 +5,13 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-    'hostname' => 'mysql.railway.internal',
-    'username' => 'root',
-    'password' => 'eBBazdhuCQD1drrVvCoRuJCNjmzTBbXO',
-    'database' => 'railway',
-    'dbdriver' => 'mysqli',
-    'port'     => '13292',
+    'dsn'   => 'mysqli://root:eBBazdhuCQD1drrVvCoRuJCNjmzTBbXO@trolley.proxy.rlwy.net:13292/railway',
+    'hostname' => '',
+    'username' => '',
+    'password' => '',
+    'database' => '',
+    'port'     => '',
+    'dbdriver' => 'mysqli'
     'dbprefix' => '',
     'pconnect' => FALSE,
     'db_debug' => (ENVIRONMENT !== 'production'),
