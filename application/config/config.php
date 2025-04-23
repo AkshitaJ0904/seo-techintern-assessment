5a9480670fb5a30d37b68e410e2cbe1fc55ec1db
Akshita Jain <144626106+AkshitaJ0904@users.noreply.github.com>
2025-04-23 20:38:51 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/product-listing-platform/';
+$config['base_url'] = 'https://web-production-49134.up.railway.app/';
 
 /*
 |--------------------------------------------------------------------------
