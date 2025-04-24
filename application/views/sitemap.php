93f4568e87afaf042999ff9dde70c9aedc673124
Dhanush kumar <dhanush.23cse@bmu.edu.in>
2025-04-24 11:27:32 +0530

    Update sitemap.php

--- application/views/sitemap.php
+++ application/views/sitemap.php
@@ -1,4 +1,10 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8" ?>'; ?>
+<?php
+// Set the correct content type for XML
+header("Content-Type: application/xml; charset=utf-8");
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <!-- Homepage -->
     <url>
@@ -8,20 +14,20 @@
     </url>
     
     <!-- Categories -->
-    <?php foreach($categories as $category): ?>
-    <url>
-        <loc><?= base_url('products/' . $category->slug); ?></loc>
-        <priority>0.8</priority>
-        <changefreq>weekly</changefreq>
-    </url>
+    <?php foreach ($categories as $category): ?>
+        <url>
+            <loc><?= base_url('products/' . htmlspecialchars($category->slug)); ?></loc>
+            <priority>0.8</priority>
+            <changefreq>weekly</changefreq>
+        </url>
     <?php endforeach; ?>
     
     <!-- Products -->
-    <?php foreach($products as $product): ?>
-    <url>
-        <loc><?= base_url('product/' . $product->slug); ?></loc>
-        <priority>0.6</priority>
-        <changefreq>weekly</changefreq>
-    </url>
+    <?php foreach ($products as $product): ?>
+        <url>
+            <loc><?= base_url('product/' . htmlspecialchars($product->slug)); ?></loc>
+            <priority>0.6</priority>
+            <changefreq>weekly</changefreq>
+        </url>
     <?php endforeach; ?>
 </urlset>
